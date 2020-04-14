cc560ea1eeaa96c71dd911ca378f3986d48da2f1
Pietro Passarelli <pietro.passarelli@wsj.com>
2020-04-14 10:12:27 -0400

    Updates

---
+++ _next/static/NEbqZZaL-lBs3EuJhmlCP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/books/[bookId]":["static\u002Fchunks\u002F4.b2ac75bba88e9c75b2b0.js"],"/books/[bookId]/[chapterId]":[a],"/index":[a]}}("static\u002Fchunks\u002Fed5544283b83eeb0e737cfe581edb4d8f5192ced.6c44f0048d1351987336.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
