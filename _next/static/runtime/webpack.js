b5e34e4e0912ae56f74aa01a2b505294d03a5cf6
Pietro Passarelli <pietro.passarelli@wsj.com>
2020-04-14 10:15:15 -0400

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c2081d3af86c65f0e3b3";
+/******/ 	var hotCurrentHash = "92e854c1c645af31637d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
