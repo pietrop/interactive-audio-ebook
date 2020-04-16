26a968d33115bae4c275c03c9cdafc55437c77c1
Pietro Passarelli <pietro.passarelli@wsj.com>
2020-04-15 23:12:09 -0400

    added a request a book card

--- pages/books/index.js
+++ pages/books/index.js
@@ -19,7 +19,7 @@ function Books() {
 
 
     { booksIndex.map(book => (
-        <Col>
+        <Col className="d-flex align-items-stretch">
             <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src={`https://www.gutenberg.org/files/${book.gutembergId}/${book.gutembergId}-h/images/bookcover.jpg`} fluid />
             <Card.Body>
@@ -27,32 +27,36 @@ function Books() {
               <Card.Text title={book.description}>
                 {book.description.substr(0, 200)}...
               </Card.Text>
-              <Link href={`${basePath}/books/[bookId]`} as={`${basePath}/books/${book.librivoxBookId}`}>
+            </Card.Body>
+            <Card.Footer>
+            <Link href={`${basePath}/books/[bookId]`} as={`${basePath}/books/${book.librivoxBookId}`}>
                <Button variant="primary" block>Listen/Read</Button>
               </Link>
-            </Card.Body>
+            </Card.Footer>
             </Card>
           <br/>
         </Col>
       ))
     }
 
-<Col>
+          <Col  className="d-flex align-items-stretch">
             <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src="https://via.placeholder.com/750x750" fluid />
             <Card.Body>
               <Card.Title> Book sudgestion </Card.Title>
               <Card.Text>
                 if you have a sudgestion for a book, from Librivox/Gutemberg that you'd like to see in here, feel free to raise an issue in github.
-
+        
               {/* Adventures of Huckleberry Finn (1884) by Mark Twain is one of the truly great American novels, beloved by children, adults, and literary critics alike. The book tells the story of “Huck” Finn (first ... */}
               </Card.Text>
-              <a href={`https://github.com/pietrop/interactive-audio-ebook/issues/new?assignees=pietrop&labels=new+book+request&template=new-audio-ebook-request.md&title=`} target="_blank" rel="noopener">
-               <Button variant="primary" block>Github issues</Button>
-              </a>
             </Card.Body>
+            <Card.Footer>
+            <a href={`https://github.com/pietrop/interactive-audio-ebook/issues/new?assignees=pietrop&labels=new+book+request&template=new-audio-ebook-request.md&title=`} target="_blank" rel="noopener">
+               <Button variant="primary" block>Request a book</Button>
+              </a>
+            </Card.Footer>
             </Card>
-          <br/>
+            <br/>     
         </Col>
     </Row>
 
